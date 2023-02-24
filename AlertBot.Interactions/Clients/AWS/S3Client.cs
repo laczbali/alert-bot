fdcb8dd85aa21634a0fd186509bf82818d7a3333
Balázs Laczkó <laczbali@gmail.com>
2023-02-24 22:25:15 +0100

    removing some scaffolding

--- AlertBot.Interactions/Clients/AWS/S3Client.cs
+++ AlertBot.Interactions/Clients/AWS/S3Client.cs
@@ -2,16 +2,6 @@
 {
 	public class S3Client
 	{
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="contents">Raw file contents</param>
-		/// <param name="fullFileName">The name of the file to store, including the extension</param>
-		/// <returns>Text File URL</returns>
-		public async Task<string> StoreExpiringText(string contents, string fullFileName)
-		{
-			// https://aws.amazon.com/blogs/aws/amazon-s3-object-expiration/
-			throw new NotImplementedException();
-		}
+		
 	}
 }
