bcb9e9724d8e055ea8d7c69b93ced9834ac8f992
Balázs Laczkó <laczbali@gmail.com>
2023-02-24 22:32:41 +0100

    added notes

--- AlertBot.Interactions/Clients/Twilio/TwilioClient.cs
+++ AlertBot.Interactions/Clients/Twilio/TwilioClient.cs
@@ -9,6 +9,12 @@
 
         public async Task SendVoiceMessage(string contents, string targetPhoneNumber)
         {
+            // Flow
+            // 1 - A GUID gets generated
+            // 2 - We upload GUID.xml to S3
+            // 3 - We return this URL: azurefunction/voice/GUID.xml
+            // 4 - Twilio hits a controller here (HTTP POST)
+            // 5 - We fetch the file from S3, return it to Twilio and delete it from S3
 			throw new NotImplementedException();
 		}
     }
